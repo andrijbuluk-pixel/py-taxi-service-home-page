99ad39d1492607c2ec1351200ec6fb1ac0a75aa4
Andriy <andrijbuluk@gmail.com>
2026-04-16 16:56:02 +0200

    fix flake8

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,7 +114,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR / "static"),
 ]
 
